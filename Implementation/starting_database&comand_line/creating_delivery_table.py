8c7f1db9399c36c8fd1a45c4371e945fd6d39ba4
robert cook <31062@longroad.ac.uk>
2014-12-10 11:11:58 +0000

    starting to cretae the database fineshed norm.

---
+++ Implementation/starting_database&comand_line/creating_delivery_table.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+#open/create new database
+with sqlite3.connect("pub_stock.db") as db:
+    #make the cursor
+    cursor = db.cursor()
+    #create sql
+    sql = """create table delivery(
+        DeliveryID integer,
+        DeliveryDateTime text,
+        UserLastName text,
+        UserPhoneNumber text,
+        UserEmailAddress text,
+        UserCity text,
+        UserStreet text,
+        UserHouseNo text,
+        UserPostcode text,
+        primary key(UserID))"""
+    #execute sql code
+    cursor.execute(sql)
+    #commit to database
+    db.commit()
