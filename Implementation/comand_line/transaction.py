759da31fceb6294d86acab0c12b5368c923c7579
robert cook <31062@longroad.ac.uk>
2015-01-29 13:53:32 +0000

    29/1/15

--- Implementation/comand_line/transaction.py
+++ Implementation/comand_line/transaction.py
@@ -1,4 +1,5 @@
 import sqlite3
+import datetime
 def transaction_user():
     with sqlite3.connect("pub_stock.db") as db:
         cursor = db.cursor()
@@ -35,37 +36,47 @@ def transaction_product():
         except ValueError:
             print("datatype error")
             check_2 = True
+    return productID_list
 
 def transaction_insert_transaction(user_ID):
-    ts = datetime.datetime.now()
-    print(ts)
+    time_stamp = datetime.datetime.now()
     values = (time_stamp,user_ID)
     with sqlite3.connect("pub_stock.db") as db:
         cursor = db.cursor()
-        sql = """insert into Transactions(TransactionsTimeDateID,UserID) values(?,?)"""
+        sql = """insert into Transactions(TransactionsTimeDate,UserID) values(?,?)"""
         cursor.execute(sql,values)
         db.commit()
     
-def transaction_insert_transactionproduct():
+def transaction_insert_transactionproduct(productID_list):
     with sqlite3.connect("pub_stock.db") as db:
             cursor = db.cursor()
-            cursor.execute("select TransactionsID from Transactions order desc")
-            cursor.execute("select TransactionsID from transactions")
-            transaction_id = cursor.fetchone()
-    for each in productID_list:
-        values = (productID_list[each],transaction_id)
+            cursor.execute("select max(TransactionsID) from transactions")
+            transaction_id = cursor.fetchall()
+            transaction_id = (transaction_id[0])
+            
+    check = True
+    point = len(productID_list)
+    pointer = 0
+    while check:
+        product_ID = productID_list[pointer]
+        values = (product_ID,transaction_id)
+        print(product_ID,transaction_id)
         with sqlite3.connect("pub_stock.db") as db:
             cursor = db.cursor()
             sql = """insert into TransactionsProduct(ProductID,TransactionsID) values(?,?)"""
             cursor.execute(sql,values)
             db.commit()
+    pointer += 1
+    point -= 1
+    if point == 0:
+        check = False
     
 def transaction_main():
     print("----TRANSACTION----")
     user_ID = transaction_user()
-    transaction_product()
+    productID_list = transaction_product()
     transaction_insert_transaction(user_ID)
-    transaction_insert_transactionproduct()
+    transaction_insert_transactionproduct(productID_list)
     print("----TRANSACTION_COMPLETE----")
 
 if __name__=="__main__":
